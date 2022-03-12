c733414dfb3efba0fe882ebea637a3ce05837714
M@ttPbr <65223267+mattheory@users.noreply.github.com>
2022-03-12 18:03:47 +0800

    removed sudo
    No need for sudo commands in bootstrap as system is already executing as "root"

--- bootstrap-httpd-from-s3.sh
+++ bootstrap-httpd-from-s3.sh
@@ -3,4 +3,4 @@ yum update -y #install all updates
 yum install httpd -y #install Apache web server
 systemctl start httpd #start the Apache web server
 systemctl enable httpd #enable httpd auto-start after reboot
-sudo aws s3 cp s3://<BUCKET-NAME>/ /var/www/html/ --recursive #copy website on server
+aws s3 cp s3://<BUCKET-NAME>/ /var/www/html/ --recursive #copy website on server
