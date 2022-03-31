2564e9ef7dee445c041115cbe8e8d10550afb5e2
M@ttPbr <65223267+mattheory@users.noreply.github.com>
2022-03-31 21:02:03 +0800

    added comment

--- aws-s3-to-s3-copy.sh
+++ aws-s3-to-s3-copy.sh
@@ -1,2 +1,3 @@
 #ensure ec2 instance has role with S3 Full Access & KMS Full Access for transfer
+#use '\' before spaces in folder names
 aws s3 sync s3://source-bucket-name/subfolder/ s3://destination-bucket-name/subfolder/
