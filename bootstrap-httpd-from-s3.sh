97165efd4075d80180284dcf5ac9916a3acb978e
M@ttPbr <65223267+mattheory@users.noreply.github.com>
2022-07-07 11:24:02 +0800

    Update bootstrap-httpd-from-s3.sh

--- bootstrap-httpd-from-s3.sh
+++ bootstrap-httpd-from-s3.sh
@@ -4,3 +4,4 @@ yum install httpd -y #install Apache web server
 systemctl start httpd #start the Apache web server
 systemctl enable httpd #enable httpd auto-start after reboot
 aws s3 cp s3://<BUCKET-NAME>/ /var/www/html/ --recursive #copy website on server
+#note that ec2 instance must have valid role for s3 access
