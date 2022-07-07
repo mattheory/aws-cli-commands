242c4b39c9bc2da0149801e2564933f06143f54e
M@ttPbr <65223267+mattheory@users.noreply.github.com>
2022-07-07 10:59:45 +0800

    Update bootstrap-efs-utils.sh

--- bootstrap-efs-utils.sh
+++ bootstrap-efs-utils.sh
@@ -3,3 +3,5 @@ yum update -y
 yum install -y amazon-efs-utils
 mkdir /mnt/efs/
 mount -t efs -o tls fs-05f2252940eeae14f:/ /mnt/efs/ #replace fs-05f2252940eeae14f by the efs you created
+
+#Warning: make sure you attach a security group to the efs that allows TCP to NFS port 2049 from sg of the ec2s you want to mount from
