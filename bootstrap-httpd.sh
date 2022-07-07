537577b7b38bc90b80c9ac3c45ea443d78fb82c7
M@ttPbr <65223267+mattheory@users.noreply.github.com>
2022-07-07 11:26:13 +0800

    Update bootstrap-httpd.sh

--- bootstrap-httpd.sh
+++ bootstrap-httpd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum update -y #install all updates
 yum install httpd -y #install Apache web server
+yum install git -y #install git
 echo "<html><body>This is matt's web server<h1>/h1</body></html>" > /var/ww/html/index.html #create index.html
 systemctl start httpd #start the Apache web server
 systemctl enable httpd #enable httpd auto-start after reboot
